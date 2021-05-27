b37121017e70318f9bd8e254e6ca54234ccb3e95
hugovallada <valladahugo@gmail.com>
2021-05-27 10:30:29 -0300

    Cria a entidade turma

---
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/turma/TurmaController.java
@@ -0,0 +1,13 @@
+package br.com.zupedu.dojo.ot4dojo.turma;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/turmas")
+public class TurmaController {
+
+    public ResponseEntity<>
+
+}
