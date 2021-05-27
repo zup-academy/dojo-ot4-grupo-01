9a4e8a31f8ee1106f6c29161b48cc2be5b178359
IagoFaria <iagofaria13@gmail.com>
2021-05-27 11:27:19 -0300

    Finaliza a funcionalidade básica do Controller

--- src/main/java/br/com/zupedu/dojo/ot4dojo/turma/TurmaController.java
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/turma/TurmaController.java
@@ -1,13 +1,13 @@
 package br.com.zupedu.dojo.ot4dojo.turma;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.imageio.plugins.jpeg.JPEGImageReadParam;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.validation.Valid;
@@ -15,19 +15,20 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/turmas")
+@Transactional
 public class TurmaController {
 
 	@PersistenceContext
 	private EntityManager manager;
 
 	@PostMapping
-    public ResponseEntity<?> criaTurma(@RequestBody @Valid TurmaRequest request,
+    public ResponseEntity<TurmaResponse> criaTurma(@RequestBody @Valid TurmaRequest request,
 									   UriComponentsBuilder builder) {
 
 		Turma turma = request.toModel();
 		manager.persist(turma);
 		URI uri = builder.path("/turmas/{id}").build(turma.getId());
-		return ResponseEntity.created(uri).body(null);
+		return ResponseEntity.created(uri).body(new TurmaResponse(turma));
     }
 
 }
