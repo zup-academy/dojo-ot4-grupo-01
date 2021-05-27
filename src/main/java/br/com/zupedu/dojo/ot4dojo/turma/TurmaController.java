5a0295273d0da7cfb387e1d9cdce209d3bcec246
andreevilela <andrevibegoogle@hotmail.com>
2021-05-27 10:45:50 -0300

    Cria Controller e DTO

--- src/main/java/br/com/zupedu/dojo/ot4dojo/turma/TurmaController.java
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/turma/TurmaController.java
@@ -1,13 +1,23 @@
 package br.com.zupedu.dojo.ot4dojo.turma;
 
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/turmas")
 public class TurmaController {
-
-    public ResponseEntity<>
+	
+	@PostMapping
+    public ResponseEntity<?> criaTurma(@RequestBody @Valid TurmaRequest request, UriComponentsBuilder builder) {
+    	
+		URI uri = UriComponentsBuilder.path("/turmas/{id}").build(id);
+		return ResponseEntity.created(null).body(null);
+    }
 
 }
---
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/turma/TurmaRequest.java
@@ -0,0 +1,37 @@
+package br.com.zupedu.dojo.ot4dojo.turma;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+
+public class TurmaRequest {
+	
+	@NotBlank
+	@Length(max = 50)
+	private String nome;
+	
+	@NotNull
+	private LocalDate iniciaEm;
+	
+	@NotNull
+	private LocalDate terminaEm;
+
+	public TurmaRequest(@NotBlank @Length(max = 50) String nome, @NotNull LocalDate iniciaEm,
+			@NotNull LocalDate terminaEm) {
+		super();
+		this.nome = nome;
+		this.iniciaEm = iniciaEm;
+		this.terminaEm = terminaEm;
+	}
+	
+	public Turma toModel() {
+		return new Turma()
+	}
+	
+	
+
+}
