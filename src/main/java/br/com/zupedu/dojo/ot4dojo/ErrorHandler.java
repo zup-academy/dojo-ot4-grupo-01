86a68fe2d891e975a15d899d5b35903f514239e4
Gabrielli Correa <gabriellicorrea7@gmail.com>
2021-05-27 13:06:39 -0300

    Implementação inicial do erro handler

--- src/main/java/br/com/zupedu/dojo/ot4dojo/ErrorHandler.java
+++ src/main/java/br/com/zupedu/dojo/ot4dojo/ErrorHandler.java
@@ -1,5 +1,27 @@
 package br.com.zupedu.dojo.ot4dojo;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 public class ErrorHandler {
-    public void validationError(){}
+	
+	@ExceptionHandler
+    public void validationError(MethodArgumentNotValidException exception){
+		
+		ApiError apiError = new ApiError();
+		
+		exception.getFieldErrors().forEach(field -> apiError.add(field.getField(), 
+				field.getDefaultMessage()));
+		
+	}
+	class ApiError{
+
+		void add(String field, String defaultMessage) {
+			
+			
+			
+			return null;
+		}
+		
+	}
 }
